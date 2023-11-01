b9aa8d0cdb71188d0cedf48646fff67e4b1998a0
Caio Camargo <caiocbahia@hotmail.com>
2023-11-01 16:17:47 -0300

    chore: commit before adding material

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
 
+import {CdkDragDrop, moveItemInArray} from '@angular/cdk/drag-drop';
+
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.scss']
 })
 export class MainComponent {
+
+  leadRoles = [
+    'Role1',
+    'Role2',
+    'Role3',
+  ];
+
+  drop(event: CdkDragDrop<string[]>){
+    moveItemInArray(this.leadRoles, event.previousIndex, event.currentIndex)
+  }
 
 }
