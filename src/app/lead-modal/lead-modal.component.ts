ad43a02a35806fe878111da8b8b4f1d05fbf46ad
Caio Camargo <caiocbahia@hotmail.com>
2023-11-02 22:01:41 -0300

    chore: successfully passing data between components

--- src/app/lead-modal/lead-modal.component.ts
+++ src/app/lead-modal/lead-modal.component.ts
@@ -6,6 +6,7 @@ import {
   ViewChildren,
 } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-lead-modal',
@@ -13,11 +14,58 @@ import { MatDialogRef } from '@angular/material/dialog';
   styleUrls: ['./lead-modal.component.scss'],
 })
 export class LeadModalComponent {
-  @ViewChild('selectAllBoxes')
-  selectAllRef!: HTMLInputElement;
-
   @ViewChildren('selectOption')
   selectOptionRef!: QueryList<any>;
+
+  @ViewChild('nomeCompleto')
+  usernameElement!: ElementRef;
+
+  @ViewChild('userEmail')
+  userEmailElement!: ElementRef;
+
+  @ViewChild('phoneNumber')
+  userPhoneNumberElement!: ElementRef;
+
+  //testando service
+
+  constructor(
+    public dialogRef: MatDialogRef<LeadModalComponent>,
+    private dataService: DataService
+  ) {}
+
+
+  // data: any;
+
+  // sendDataToService() {
+  //   this.dataService.setData(this.data);
+  // }
+
+  //testando service - fim
+
+  saveLeadData(): void {
+    // console.log('Clicou em Salvar', this.usernameElement.nativeElement.value);
+    const userName = this.usernameElement.nativeElement.value;
+    const userEmail = this.userEmailElement.nativeElement.value;
+    const userPhoneNumber = this.userPhoneNumberElement.nativeElement.value;
+    // console.log(userName, userEmail, userPhoneNumber);
+
+    const userData = [userName, userEmail, userPhoneNumber];
+    this.dialogRef.close();
+
+    this.dataService.setData(userData[0])
+  }
+
+
+
+
+  // data: any = this.saveLeadData()
+
+  // sendDataToService() {
+  //   this.dataService.setData(this.data);
+  // }
+
+
+
 
   changeCheckBoxValue(event: any) {
     const isChecked = event.srcElement.checked;
@@ -31,12 +79,10 @@ export class LeadModalComponent {
 
   selectAll() {
     const checkboxArray = this.selectOptionRef.toArray();
-
     checkboxArray[0].nativeElement.checked = true;
     checkboxArray[1].nativeElement.checked = true;
     checkboxArray[2].nativeElement.checked = true;
     checkboxArray[3].nativeElement.checked = true;
-    console.log('Função chamada', checkboxArray);
   }
 
   deselectAll() {
@@ -47,9 +93,11 @@ export class LeadModalComponent {
     checkboxArray[3].nativeElement.checked = false;
   }
 
-  constructor(public dialogRef: MatDialogRef<LeadModalComponent>) {}
-
   cancel(): void {
     this.dialogRef.close();
   }
 }
+function sendDataToService() {
+  throw new Error('Function not implemented.');
+}
+
--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,39 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
 import { MatDialog } from '@angular/material/dialog';
 import { LeadModalComponent } from '../lead-modal/lead-modal.component';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.scss'],
 })
-export class MainComponent {
-  constructor(public dialog: MatDialog) {}
+export class MainComponent implements OnInit {
+  constructor(public dialog: MatDialog, private dataService: DataService) {}
 
-  addLead(): void{
-    const dialogRef = this.dialog.open(LeadModalComponent, {
-      width: '500px',
-      height: '500px',
-      panelClass: 'custom-modalbox'
-    })
-
-    dialogRef.afterClosed().subscribe(result => {
-      console.log('Dialog was closed')
-    })
+  ngOnInit() {
+    this.dataService.currentData.subscribe((data) => {
+      console.log(data);
+      this.leadRoles = [data];
+    });
   }
 
-
-
-
-
-
-
-
-  leadRoles = ['Role1', 'Role2', 'Role3'];
+  leadRoles: any;
 
   drop(event: CdkDragDrop<string[]>) {
     moveItemInArray(this.leadRoles, event.previousIndex, event.currentIndex);
+  }
+
+  // ACIONAR O MODAL DE ADICIONAR LEAD
+
+  addLead(): void {
+    const dialogRef = this.dialog.open(LeadModalComponent, {
+      width: '500px',
+      height: '500px',
+      panelClass: 'custom-modalbox',
+    });
+
+    dialogRef.afterClosed().subscribe((result) => {
+      console.log('Dialog was closed');
+    });
   }
 }
